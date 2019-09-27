8c035f418851886737288e1dcbcbc98a7677063f
park kyung suk <boardguy1024@gmail.com>
2019-09-28 01:38:34 +0900

    Updates

---
+++ precache-manifest.835ca5b191cd9438048ab7a9299ae534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88a7aa1d2f161609221ec6fc517562d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3998356fefb485f3a5a1",
+    "url": "/monsters-rolodex/static/css/main.8fc0d6c8.chunk.css"
+  },
+  {
+    "revision": "d13a353a0fe4879286ec",
+    "url": "/monsters-rolodex/static/js/2.d395d0d5.chunk.js"
+  },
+  {
+    "revision": "3998356fefb485f3a5a1",
+    "url": "/monsters-rolodex/static/js/main.e02f410f.chunk.js"
+  },
+  {
+    "revision": "f9d70e3af54ddae19106",
+    "url": "/monsters-rolodex/static/js/runtime-main.e20afc0a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monsters-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
